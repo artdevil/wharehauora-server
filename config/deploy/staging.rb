009815c4a51df0240ecf9937df5b130eba99b677
adhiguna sabril <adhiguna.sabril@gmail.com>
2019-04-03 15:01:41 +0800

    update deplot

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,7 @@
 server '104.248.153.164', port: 22, roles: [:web, :app, :db], primary: true
 
 set :stage,     :staging
-set :branch,    :capistrano_deploy
+set :branch,    :master
 set :rails_env, :staging
 set :rack_env,  :staging
 set :puma_env,  :staging
@@ -14,5 +14,5 @@ set :ssh_options, {
   forward_agent: true,
   user: 'rails',
   auth_methods: %w[publickey],
-  keys: ENV['WHAREHAUORA_KEY_PEM']
+  keys: ENV['WHAREHAUORA_STAGING_KEY_PEM']
 }
