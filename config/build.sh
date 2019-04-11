4912779475d6790fc31634921ede9a46c59bd7db
adhiguna sabril <adhiguna.sabril@gmail.com>
2019-04-11 17:19:22 +0800

    update try deploy

--- config/build.sh
+++ config/build.sh
@@ -9,4 +9,4 @@ STAGE="$1"
 cd $TRAVIS_BUILD_DIR
 openssl aes-256-cbc -k $DEPLOY_KEY -in $TRAVIS_BUILD_DIR/config/deploy_id_rsa_enc_travis -d -a -out $TRAVIS_BUILD_DIR/config/deploy_id_rsa
 export WHAREHAUORA_STAGING_KEY_PEM=$TRAVIS_BUILD_DIR/config/deploy_id_rsa
-bundle exec cap $STAGE deploy
+RAILS_ENV=development bundle exec cap $STAGE deploy
