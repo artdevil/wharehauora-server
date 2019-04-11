c11103c1ab7fecf3c23b62b04399ce8d17fdb4dd
adhiguna sabril <adhiguna.sabril@gmail.com>
2019-04-11 17:36:36 +0800

    adding bundle install

--- config/build.sh
+++ config/build.sh
@@ -8,4 +8,5 @@ STAGE="$1"
 
 openssl aes-256-cbc -k $DEPLOY_KEY -in config/deploy_id_rsa_enc_travis -d -a -out config/deploy_id_rsa
 export WHAREHAUORA_STAGING_KEY_PEM=$TRAVIS_BUILD_DIR/config/deploy_id_rsa
+bundle install
 bundle exec cap $STAGE deploy
