ff63c50bdb9075e0458cd38ab6eaacaf8bef1441
adhiguna sabril <adhiguna.sabril@gmail.com>
2019-04-11 16:57:48 +0800

    update directory

--- config/build.sh
+++ config/build.sh
@@ -7,6 +7,6 @@ APP="$0"
 STAGE="$1"
 
 cd $TRAVIS_BUILD_DIR
-openssl aes-256-cbc -k $DEPLOY_KEY -in config/deploy_id_rsa_enc_travis -d -a -out config/deploy_id_rsa
-export WHAREHAUORA_STAGING_KEY_PEM=config/deploy_id_rsa
+openssl aes-256-cbc -k $DEPLOY_KEY -in $TRAVIS_BUILD_DIR/config/deploy_id_rsa_enc_travis -d -a -out $TRAVIS_BUILD_DIR/config/deploy_id_rsa
+export WHAREHAUORA_STAGING_KEY_PEM=$TRAVIS_BUILD_DIR/config/deploy_id_rsa
 bundle exec cap $STAGE deploy
