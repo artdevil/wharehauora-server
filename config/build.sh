c6489e0d3f5c91e6c3e78a0aaa020370d078fc59
adhiguna sabril <adhiguna.sabril@gmail.com>
2019-04-11 16:52:27 +0800

    move with cd

--- config/build.sh
+++ config/build.sh
@@ -6,6 +6,7 @@
 APP="$0"
 STAGE="$1"
 
+cd $TRAVIS_BUILD_DIR
 openssl aes-256-cbc -k $DEPLOY_KEY -in config/deploy_id_rsa_enc_travis -d -a -out config/deploy_id_rsa
 export WHAREHAUORA_STAGING_KEY_PEM=config/deploy_id_rsa
 bundle exec cap $STAGE deploy
