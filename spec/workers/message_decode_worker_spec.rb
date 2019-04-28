63a436ea85bd97c5486fa77e46917e945f6690df
adhiguna sabril <adhiguna.sabril@gmail.com>
2019-04-28 15:26:54 +0800

    update for testing

--- spec/workers/message_decode_worker_spec.rb
+++ spec/workers/message_decode_worker_spec.rb
@@ -4,24 +4,46 @@ RSpec.describe MessageDecodeWorker, type: :worker do
   it { is_expected.to be_processed_in :message_decode }
 
   describe 'proccesing topic' do
-    let(:home) { FactoryBot.create :home, gateway_mac_address: 'ABC1234567' }
+    describe 'with invalid home' do
+      it "not enqueues a v1 topic" do
+        expect{ 
+          MessageDecodeWorker.new.perform("/sensors/v3/1234/ABC1234567/ABC123/1/1/0/0", 120) 
+        }.to output("Couldn't find Home\n").to_stdout
+      end
 
-    it "enqueues a v1 topic" do
-      MessageDecodeWorker.perform_async("/sensors/wharehauora/#{home.id}/ABC123/1/1/0/0", 120)
-      
-      expect(MessageDecodeWorker.jobs.size).to eq(1)
+      it "not enqueues a v2 topic" do
+        expect{ 
+          MessageDecodeWorker.new.perform("/sensors/v2/ABC1234567/ABC123/1/1/0/0", 120) 
+        }.to output("Couldn't find Home\n").to_stdout
+      end
+
+      it "not enqueues a v3 topic" do
+        expect{ 
+          MessageDecodeWorker.new.perform("/sensors/v3/1234/ABC1234567/ABC123/1/1/0/0", 120) 
+        }.to output("Couldn't find Home\n").to_stdout
+      end
     end
 
-    it "enqueues a v2 topic" do
-      MessageDecodeWorker.perform_async("/sensors/v2/#{home.gateway_mac_address}/ABC123/1/1/0/0", 120)
-      
-      expect(MessageDecodeWorker.jobs.size).to eq(1)
-    end
+    describe 'with valid home' do
+      let(:home) { FactoryBot.create :home, gateway_mac_address: 'ABC1234567' }
 
-    it "enqueues a v3 topic" do
-      MessageDecodeWorker.perform_async("/sensors/v3/#{home.id}/#{home.gateway_mac_address}/ABC123/1/1/0/0", 120)
-      
-      expect(MessageDecodeWorker.jobs.size).to eq(1)
+      it "enqueues a v1 topic" do
+        MessageDecodeWorker.perform_async("/sensors/wharehauora/#{home.id}/ABC123/1/1/0/0", 120)
+        
+        expect(MessageDecodeWorker.jobs.size).to eq(1)
+      end
+
+      it "enqueues a v2 topic" do
+        MessageDecodeWorker.perform_async("/sensors/v2/#{home.gateway_mac_address}/ABC123/1/1/0/0", 120)
+        
+        expect(MessageDecodeWorker.jobs.size).to eq(1)
+      end
+
+      it "enqueues a v3 topic" do
+        MessageDecodeWorker.perform_async("/sensors/v3/#{home.id}/#{home.gateway_mac_address}/ABC123/1/1/0/0", 120)
+        
+        expect(MessageDecodeWorker.jobs.size).to eq(1)
+      end
     end
   end
 end
