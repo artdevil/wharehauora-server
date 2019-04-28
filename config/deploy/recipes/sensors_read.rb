330df8cd3d571793a5e67e2255a8b443864cb93f
adhiguna sabril <adhiguna.sabril@gmail.com>
2019-04-28 14:31:45 +0800

    update sensor

--- config/deploy/recipes/sensors_read.rb
+++ config/deploy/recipes/sensors_read.rb
@@ -5,17 +5,17 @@ namespace :sensors_read do
     "#{shared_path}/tmp/pids/sensors_read.pid"
   end
 
-  def pid_file_exists?
+  def pid_sensor_file_exists?
     test(*("[ -f #{sensors_read_pid} ]").split(' '))
   end
 
-  def pid_process_exists?
-    pid_file_exists? and test(*("kill -0 $( cat #{sensors_read_pid} )").split(' '))
+  def pid_sensor_process_exists?
+    pid_sensor_file_exists? and test(*("kill -0 $( cat #{sensors_read_pid} )").split(' '))
   end
 
   task :start do
     on roles(:app) do
-      if !pid_process_exists?
+      if !pid_sensor_process_exists?
         # https://stackoverflow.com/questions/10889621/launching-background-process-in-capistrano-task/34622311
         execute "cd #{release_path} && (nohup #{fetch(:rvm_path)}/bin/rvm default do bundle exec rake sensors:ingest BACKGROUND=true PIDFILE=#{sensors_read_pid} RAILS_ENV=#{fetch(:rails_env)} &) && sleep 1"
       end
@@ -24,7 +24,7 @@ namespace :sensors_read do
 
   task :stop do
     on roles(:app) do
-      if pid_process_exists?
+      if pid_sensor_process_exists?
         execute "kill `cat #{sensors_read_pid}`"
         execute "rm #{sensors_read_pid}"
       end
--- config/deploy/recipes/sidekiq.rb
+++ config/deploy/recipes/sidekiq.rb
@@ -1,34 +1,30 @@
 # http://blog.blakesimpson.co.uk/read/80-sidekiq-tasks-for-capistrano-3
-# after 'pumactl:restart', 'sidekiq:restart'
+after 'pumactl:restart', 'sidekiq:restart'
 
 namespace :sidekiq do
   def sidekiq_pid
     "#{shared_path}/tmp/pids/sidekiq.pid"
   end
 
-  def pid_file_exists?
+  def pid_sidekiq_file_exists?
     test(*("[ -f #{sidekiq_pid} ]").split(' '))
   end
 
-  def pid_process_exists?
-    pid_file_exists? and test(*("kill -0 $( cat #{sidekiq_pid} )").split(' '))
+  def pid_sidekiq_process_exists?
+    pid_sidekiq_file_exists? and test(*("kill -0 $( cat #{sidekiq_pid} )").split(' '))
   end
 
   task :start do
     on roles(:app) do
-      if !pid_process_exists?
-        with RAILS_ENV: fetch(:environment) do
-          within "#{fetch(:deploy_to)}/current/" do
-            execute :bundle, :exec, "sidekiq -C config/sidekiq.yml -e #{fetch(:rails_env)} -L #{fetch(:deploy_to)}/shared/log/sidekiq.log -P #{sidekiq_pid} -d"
-          end
-        end
+      if !pid_sidekiq_process_exists?
+        execute "cd #{release_path} && (nohup #{fetch(:rvm_path)}/bin/rvm default do bundle exec sidekiq -C config/sidekiq.yml -e #{fetch(:rails_env)} -L log/sidekiq.log -P #{sidekiq_pid} -d &) && sleep 1"
       end
     end
   end
 
   task :stop do
     on roles(:app) do
-      if pid_process_exists?
+      if pid_sidekiq_process_exists?
         execute "kill `cat #{sidekiq_pid}`"
         execute "rm #{sidekiq_pid}"
       end
