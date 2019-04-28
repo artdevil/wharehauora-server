800085f3261396fef847e73c4837fb4ca072892b
adhiguna sabril <adhiguna.sabril@gmail.com>
2019-04-28 08:59:01 +0800

    update sidekiq testing

--- config/deploy.rb
+++ config/deploy.rb
@@ -152,3 +152,48 @@ namespace :sensors_read do
 end
 
 after 'pumactl:restart', 'sensors_read:restart'
+
+# http://blog.blakesimpson.co.uk/read/80-sidekiq-tasks-for-capistrano-3
+namespace :sidekiq do
+  def sidekiq_pid
+    "#{shared_path}/tmp/pids/sidekiq.pid"
+  end
+
+  def pid_file_exists?
+    test(*("[ -f #{sidekiq_pid} ]").split(' '))
+  end
+
+  def pid_process_exists?
+    pid_file_exists? and test(*("kill -0 $( cat #{sidekiq_pid} )").split(' '))
+  end
+
+  task :start do
+    on roles(:app) do
+      if !pid_process_exists?
+        with RAILS_ENV: fetch(:environment) do
+          within "#{fetch(:deploy_to)}/current/" do
+            execute :bundle, :exec, "sidekiq -C config/sidekiq.yml -e #{fetch(:rails_env)} -L #{fetch(:deploy_to)}/shared/log/sidekiq.log -P #{sidekiq_pid} -d"
+          end
+        end
+      end
+    end
+  end
+
+  task :stop do
+    on roles(:app) do
+      if pid_process_exists?
+        execute "kill `cat #{sidekiq_pid}`"
+        execute "rm #{sidekiq_pid}"
+      end
+    end
+  end
+
+  task :restart do
+    on roles(:app) do
+      invoke "sidekiq:stop"
+      invoke "sidekiq:start"
+    end
+  end
+end
+
+after 'pumactl:restart', 'sidekiq:restart'
